8ee8ec925854426e9e46556be49879b3241cf137
Igor Rodionov <goruha@gmail.com>
2024-11-02 19:20:36 +0100

    Initial commit

--- src/outputs.tf
+++ src/outputs.tf
@@ -1,4 +1,19 @@
-output "mock" {
-  description = "Mock output example for the Cloud Posse Terraform component template"
-  value       = local.enabled ? "hello ${basename(abspath(path.module))}" : ""
+output "delegated_administrator_account_id" {
+  value       = local.org_delegated_administrator_account_id
+  description = "The AWS Account ID of the AWS Organization delegated administrator account"
+}
+
+output "member_account_ids" {
+  value       = local.create_org_settings ? local.member_account_id_list : null
+  description = "The AWS Account IDs of the member accounts"
+}
+
+output "macie_account_id" {
+  value       = local.create_macie_account ? try(aws_macie2_account.this[0].id, null) : null
+  description = "The ID of the Macie account created by the component"
+}
+
+output "macie_service_role_arn" {
+  value       = local.create_macie_account ? try(aws_macie2_account.this[0].service_role, null) : null
+  description = "The Amazon Resource Name (ARN) of the service-linked role that allows Macie to monitor and analyze data in AWS resources for the account."
 }
